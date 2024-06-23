e065eb60a7bd8729256f2a3b583fbb948b6229a9
Franco Montesano <134455933+frann-1911@users.noreply.github.com>
2024-06-23 19:47:02 -0300

    TERMINADO

--- pirata.c
+++ pirata.c
@@ -3,6 +3,77 @@
 #include <stdlib.h>
 #include <string.h>
 
+//declaro una funcion para imprimir el tablero 
+void imprimir(int num,char *tabla[num][num]){
+for(int i = 0; i < num;i++){
+	for(int j = 0; j < num;j++){
+		printf("%s",tabla[i][j]);
+	}
+	printf("\n");
+}	
+}
+//declaro una funcion para el juego de buscar el tesoro 
+
+void buscar(int num, char *tabla[num][num], int piratac, int pirataf, int tesoroc, int tesorof) {
+	int encontrado = 0;
+	for(int var = 0; var <= 50 && encontrado == 0; var++ ) {
+	imprimir(num,tabla);
+	//printf("la posicion  es %d %d\n", tesorof + 1, tesoroc + 1); esta linea era para saber cuando lo programabamos, donde 
+	printf("El pirata esta en la fila %d columna %d\n", pirataf, piratac);
+	tabla[pirataf][piratac] = "|-|";
+	printf("Elija la fila en la que quiere posicionarse: \n");
+	scanf("%d",&pirataf);
+	printf("Elija la columna en la que quiere posicionarse: \n");
+	scanf("%d",&piratac);
+	tabla[pirataf][piratac] = "|P|";
+		if(piratac - 1 == tesoroc && pirataf - 1 == tesorof) {
+			printf("GANASTE\n");
+			tabla[tesorof][tesoroc] = "|T|";
+			imprimir(num, tabla);
+			encontrado = 1;
+		}
+    }
+}
+
+
+//declaro una funcion para el juego de moverse hasta encontrar el tesoro
+void moverse(int num, char *tabla[num][num], int piratac, int pirataf, int tesoroc, int tesorof){
+	int encontrado = 0;
+	char movimiento;
+	for(int var = 0; var <= 50 && encontrado == 0; var++ ) {
+	imprimir(num,tabla);
+	printf("Elegi entre Norte, Sur, Este u Oeste escribiendo la inicial\n");
+	printf("%d %d\n", tesorof + 1, tesoroc + 1);
+	scanf(" %c",&movimiento);
+	if(movimiento == 'N' || movimiento == 'n'){
+		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";
+		pirataf--;
+	}else if(movimiento == 'S' || movimiento == 's'){
+		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";
+		pirataf++;
+	}else if(movimiento == 'O' || movimiento == 'o'){
+		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";
+		piratac--;
+	}else if(movimiento == 'E' || movimiento == 'e'){
+		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";	
+		piratac++;
+	}
+	tabla[pirataf][piratac] = "|P|";
+	if(piratac - 1 == tesoroc && pirataf - 1 == tesorof) {
+			printf("GANASTE\n");
+			tabla[tesorof][tesoroc] = "|T|";
+			imprimir(num, tabla);
+			encontrado = 1;
+		}
+	else if(pirataf == 0 || pirataf == num - 1 || piratac == 0 || piratac == num - 1) {
+			printf("te caiste ugu\n");
+			encontrado = 1;
+		}
+
+	
+}
+}
+
 //genero una funcion para inicializar el tablero 
 void tablero(){
 
@@ -56,73 +127,8 @@ void tablero(){
 	
 
 }
-//declaro una funcion para imprimir el tablero 
-void imprimir(int num,char *tabla[num][num]){
-for(int i = 0; i < num;i++){
-	for(int j = 0; j < num;j++){
-		printf("%s",tabla[i][j]);
-	}
-	printf("\n");
-}	
-}
-//declaro una funcion para el juego de buscar el tesoro 
-void buscar(int num, char *tabla[num][num], int piratac, int pirataf, int tesoroc, int tesorof) {
-	int encontrado = 0;
-	for(int var = 0; var <= 50 && encontrado == 0; var++ ) {
-	imprimir(num,tabla);
-	//printf("la posicion  es %d %d\n", tesorof + 1, tesoroc + 1); esta linea era para saber cuando lo programabamos, donde 
-	printf("El pirata esta en la fila %d columna %d\n", pirataf, piratac);
-	tabla[pirataf][piratac] = "|-|";
-	printf("Elija la fila en la que quiere posicionarse: \n");
-	scanf("%d",&pirataf);
-	printf("Elija la columna en la que quiere posicionarse: \n");
-	scanf("%d",&piratac);
-	tabla[pirataf][piratac] = "|P|";
-		if(piratac - 1 == tesoroc && pirataf - 1 == tesorof) {
-			printf("GANASTE\n");
-			tabla[tesorof][tesoroc] = "|T|";
-			imprimir(num, tabla);
-			encontrado = 1;
-		}
-    }
-}
-//declaro una funcion para el juego de moverse hasta encontrar el tesoro
-void moverse(int num, char *tabla[num][num], int piratac, int pirataf, int tesoroc, int tesorof){
-	int encontrado = 0;
-	char movimiento;
-	for(int var = 0; var <= 50 && encontrado == 0; var++ ) {
-	imprimir(num,tabla);
-	printf("Elegi entre Norte, Sur, Este u Oeste escribiendo la inicial\n");
-	printf("%d %d\n", tesorof + 1, tesoroc + 1);
-	scanf(" %c",&movimiento);
-	if(movimiento == 'N' || movimiento == 'n'){
-		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";
-		pirataf--;
-	}else if(movimiento == 'S' || movimiento == 's'){
-		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";
-		pirataf++;
-	}else if(movimiento == 'O' || movimiento == 'o'){
-		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";
-		piratac--;
-	}else if(movimiento == 'E' || movimiento == 'e'){
-		tabla[pirataf][piratac] = "\033[1;33m|-|\033[0m";	
-		piratac++;
-	}
-	tabla[pirataf][piratac] = "|P|";
-	if(piratac - 1 == tesoroc && pirataf - 1 == tesorof) {
-			printf("GANASTE\n");
-			tabla[tesorof][tesoroc] = "|T|";
-			imprimir(num, tabla);
-			encontrado = 1;
-		}
-	else if(pirataf == 0 || pirataf == num - 1 || piratac == 0 || piratac == num - 1) {
-			printf("te caiste ugu\n");
-			encontrado = 1;
-		}
 
-	
-}
-}
+
 
 
 
