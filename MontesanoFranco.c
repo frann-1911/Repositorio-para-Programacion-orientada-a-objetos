6776c4a2430b94428610a82bf0359e567722d771
Franco Montesano <134455933+frann-1911@users.noreply.github.com>
2024-06-24 15:14:22 -0300

    Add files via upload

--- MontesanoFranco.c
+++ MontesanoFranco.c
@@ -1,193 +1,55 @@
 #include <stdio.h>
-//use gdb compiler 
-// Aprobame profe <3
-
-int main(int argc, char *argv[]){
-    //declaro una variable para elegir a que ejercicio acceder
-int ejercicio;
-
-// variables del ejercicio 1 :)
-float kilosVendidos;
-int TipoUva;
-int TamanioUva;
-float PrecioFinal;
-
-// variables del ejercicio 2 :)
-int base;
-int potencia;
-int resultado;
-int temporal = 0;
-
-// variables del ejercicio 3 :)
-float radio;
-float pi=3.141592;
-float longitudCirc;
-float areaCirc;
-float volumenEsf;
-
-// variables del ejercicio 4 :)
-int genero;
-int edad;
-int AniosAportes;
-
-// variables del ejercicio 5 :)
-int cantidadHijos;
-int pago;
-int cantidadEscolar;
+//usé OnlineGDB
+//No esta completo
 
 
-printf("ingrese el numero de ejercicio al que quiere acceder\n");
-printf("1- Ejercicio 1\n");
-printf("2- Ejercicio 2\n");
-printf("3- Ejercicio 3\n");
-printf("3- Ejercicio 4\n");
-printf("3- Ejercicio 5\n");
-scanf("%d", &ejercicio);
+void AsignarPremios(){
+int premios[7];
+//Le pido al usuario que ingrese los valores de los premios para cada juego
+printf("Este programa decidira los ganadores de los premios. Por cada juego habrá un ganador.\n");
+printf("ingrese el valor del premio del primer juego:\n");
+scanf("%d", &premios[0]);
+printf("ingrese el valor del premio del segundo juego:\n");
+scanf("%d", &premios[1]);
+printf("ingrese el valor del premio del tercer juego:\n");
+scanf("%d", &premios[2]);
+printf("ingrese el valor del premio del cuarto juego:\n");
+scanf("%d", &premios[3]);
+printf("ingrese el valor del premio del quinto juego:\n");
+scanf("%d", &premios[4]);
+printf("ingrese el valor del premio del sexto juego \n");
+scanf("%d", &premios[5]);
+printf("ingrese el valor del premio del septimo juego:\n");
+scanf("%d", &premios[6]);
+//guardo los premios en un array, de modo que la primera posicion del array es el 
+//premio del primer juego y asi sucesivamente 
 
-switch(ejercicio){
-    //ejercicio 1
-    case 1:
-    printf("ingrese la cantidad de kilos venidos\n");
-    scanf("%f", &kilosVendidos);
-    printf("ingrese el tipo de uva\n");
-    printf("1 Uva tipo A\n");
-    printf("2 Uva tipo B\n");
-    scanf("%d", &TipoUva);
+}
 
-    //Le pido al usuario que ingrese el tipo y tamaño de la uva
-    if(TipoUva == 1){
-        printf("ingrese el tamaño de la uva\n");
-        printf("1- tamaño 1 \n");
-        printf("2- tamaño 2 \n");
-        scanf("%d", &TamanioUva);
-        if(TamanioUva == 1){
-            PrecioFinal = 200 * kilosVendidos;
-            printf("su venta fue de %f \n", PrecioFinal);
-        }
-           if(TamanioUva == 2){
-            PrecioFinal = 300 * kilosVendidos;
-            printf("su venta fue de %f \n", PrecioFinal);
-        }
+
+int main(){
+AsignarPremios();
+//le doy un maximo de 210 a los arrays porque 30 jugadores X 7 juegos es 210. Los jugadores solo podran obtener un
+
+int numeroJugador[210];
+int juego[210];
+int puntuacion[210];
+int decision;
+int num;
+
+printf("ingrese el numero de jugador, luego el numero de juego y por ultimo la puntuacion obtenida\n");
+printf("cuando desee dejar de ingresar datos, coloque un 0 en el numero de jugador \n");
+
+    do{
+scanf("%d %d %d", &numeroJugador[num], &juego[num], &puntuacion[num]);
+printf("desea ingresar otra terna?\n 0: no \t 1:si\n");
+scanf("%d", &decision);
+num ++;
     }
-    else if(TipoUva == 2){
-         printf("ingrese el tamaño de la uva\n");
-        printf("1- tamaño 1 \n");
-        printf("2- tamaño 2 \n");
-        scanf("%d", &TamanioUva);
-        if(TamanioUva == 1){
-            PrecioFinal = 300 * kilosVendidos;
-            printf("su venta fue de %f \n", PrecioFinal);
-        }
-           if(TamanioUva == 2){
-            PrecioFinal = 500 * kilosVendidos;
-            printf("su venta fue de %f \n", PrecioFinal);
-        }
-    }
-    break;
-    //ejercicio 2
-    case 2:
-    printf("ingrese 2 numeros, el primero sera la base y el segundo sera la potencia\n");
-    scanf("%d %d",  &base, &potencia);
-    if(base > 0 && base / 1 == base && potencia > 0 && potencia / 1 == potencia){
-     for (int i = 0; i < potencia; i++)
-     {        
-        resultado = base * base;
+    while(decision != 0);
 
-     }
+do
 
-     
-        printf("El resultado es %d", resultado);
-    }
-    else{
-        printf(" ingrese los datos de vuelta, ya que uno de los numeros es negativo o no es un entero");
-    }
-    break;
-
-//ejercicio 3
-    case 3:
-printf("ingrese el radio para realizar los calculos\n");
-scanf("%f", &radio);
-longitudCirc = 2*pi*radio;
-areaCirc = pi*(radio * radio);
-volumenEsf = 4/3*pi*(radio*radio*radio);
-printf("la longitud de la circumferencia es %.2f \n", longitudCirc);
-printf("el area de la circumferencia es %.2f \n", areaCirc);
-printf("el volumen de la circumferencia es %.2f \n", volumenEsf);
-    break;
-
-    case 4:
-    printf("ingrese su genero \n");
-    printf("1 hombre\n");
-    printf("2 mujer \n");
-    scanf("%d", &genero);
-    printf("ingrese su edad \n");
-    scanf("%d", &edad);
-    printf("ingrese sus años de aportes\n");
-    scanf("%d", &AniosAportes);
-    if (genero == 1)
-    {
-        if(edad >= 65){
-            if (AniosAportes >= 20)
-            {
-                printf("Usted se puede jubilar \n");
-        
-            }
-            else{
-                printf("Usted no se puede jubilar \n");
-                
-            }
-            
-        }
-        else{
-            printf("no cumple con la edad necesaria \n");
-        }
-    }
-    if (genero ==2)
-    {
-    if (edad >= 60)
-    {
-  if (AniosAportes >= 20)
-            {
-                printf("Usted se puede jubilar \n");
-        
-            }
-            else{
-                printf("Usted no se puede jubilar \n");
-                
-            }    }
-              else{
-            printf("no cumple con la edad necesaria \n");
-        }
-            }
-             break;
-
-    case 5:
-    printf("ingrese la cantidad de hijos\n");
-    scanf("%d", &cantidadHijos);
-    printf("ingrese la cantidad de hijos en edad escolar (entre 6 y 18)\n");
-    scanf("%d", &cantidadEscolar);
-    if (cantidadHijos <= 2)
-    {
-        pago = cantidadHijos * 700;
-        printf("usted recibira %d", pago+cantidadEscolar*1000);
-
-    }
-        if (cantidadHijos > 2 && cantidadHijos <= 5)
-    {
-        pago = cantidadHijos * 800;
-        printf("usted recibira %d", pago+cantidadEscolar*1000);
-
-    }
-    else if(cantidadHijos>5){
-         pago = cantidadHijos * 1200;
-        printf("usted recibira %d", pago+cantidadEscolar*1000);
-    }
-    
-    
-    break;
-    }}
-    
-      
+}
 
 
-    
